27ee9b1673e4681d7a4256ba1f1f20bff53da4e8
bpcavalcante <brunoprofetacavalcante@gmail.com>
2025-02-05 16:57:09 -0300

    Criando Projeto

---
+++ src/main/java/com/fiap/logistica_entrega/LogisticaEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.logistica_entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaEntregaApplication.class, args);
+	}
+
+}
