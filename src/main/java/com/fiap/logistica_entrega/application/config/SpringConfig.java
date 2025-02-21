e310e97d6a35d991872a4a276ccb744e0047308b
bpcavalcante <brunoprofetacavalcante@gmail.com>
2025-02-21 17:03:21 -0300

    Ajustes e swagger

--- src/main/java/com/fiap/logistica_entrega/application/config/SpringConfig.java
+++ src/main/java/com/fiap/logistica_entrega/application/config/SpringConfig.java
@@ -7,21 +7,11 @@ import com.fiap.logistica_entrega.domain.usecase.FindEntrega;
 import com.fiap.logistica_entrega.domain.usecase.UpdateEntrega;
 import com.fiap.logistica_entrega.infrastructure.EntregaRepositoryPortImpl;
 import com.fiap.logistica_entrega.infrastructure.jpa.EntregaRepository;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.sql.DataSource;
-
 @Configuration
-@EnableJpaRepositories(basePackages = "com.fiap.logistica_entrega.infrastructure.jpa")
-@EntityScan(basePackages = "com.fiap.logistica_entrega.domain")
-@EnableTransactionManagement
 public class SpringConfig implements WebMvcConfigurer {
 
     @Bean
@@ -50,12 +40,4 @@ public class SpringConfig implements WebMvcConfigurer {
         return new EntregaRepositoryPortImpl(entregaRepository);
     }
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder, DataSource dataSource) {
-        return builder
-                .dataSource(dataSource)
-                .packages("com.fiap.logistica_entrega.domain") // Adjust to your entity package
-                .persistenceUnit("logisticaPU")
-                .build();
-    }
 }
